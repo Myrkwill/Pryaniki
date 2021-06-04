3c36406f22a9c19bc965878c29cbd1f26ee07fbc
Mark Nagibin <myrkwill@icloud.com>
2021-06-04 14:50:16 +0300

    Initial Commit

---
+++ Pryaniki/PryanikiApp.swift
@@ -0,0 +1,17 @@
+//
+//  PryanikiApp.swift
+//  Pryaniki
+//
+//  Created by Mark Nagibin on 04.06.2021.
+//
+
+import SwiftUI
+
+@main
+struct PryanikiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
