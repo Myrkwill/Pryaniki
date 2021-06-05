efd897c857dc65089ef2d0206bdc71c4d8aa993c
Mark Nagibin <myrkwill@icloud.com>
2021-06-05 17:31:36 +0300

    Init

--- Pryaniki/PryanikiApp.swift
+++ Pryaniki/PryanikiApp.swift
@@ -1,14 +1,7 @@
-//
-//  PryanikiApp.swift
-//  Pryaniki
-//
-//  Created by Mark Nagibin on 04.06.2021.
-//
-
 import SwiftUI
 
 @main
-struct PryanikiApp: App {
+struct PryanikyApp: App {
     var body: some Scene {
         WindowGroup {
             ContentView()
---
+++ Pryaniki/Views/DefaultCellView.swift
@@ -0,0 +1,42 @@
+//
+//  ImageCellView.swift
+//  Pryaniki
+//
+//  Created by Mark Nagibin on 04.06.2021.
+//
+
+import SwiftUI
+
+struct DefaultCellView: View {
+    
+    @StateObject var viewModel = ImageProvider()
+    let item: SampleData    
+    @State private var showingAlert: Bool = false
+    
+    var body: some View {
+        HStack {
+            Text(item.data.text!)
+            Spacer()
+            if let url = URL(string: item.data.url ?? "") {
+                Image(uiImage: viewModel.image)
+                    .resizable()
+                    .frame(width: 30, height: 30)
+                    .onAppear {
+                        viewModel.loadImage(url: url)
+                    }
+            }
+        }
+        .alert(isPresented: $showingAlert, content: {
+            Alert(
+                title: Text(item.name),
+                message: Text(item.data.text!),
+                dismissButton: .default(Text("Ok"))
+            )
+        })
+        .onTapGesture {
+            showingAlert = true
+        }
+        .padding()
+    }
+}
+
